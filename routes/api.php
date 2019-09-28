68429aa70bfe8a81cce22ce2eba1312aea7de23a
huaerjiejuxiong <2656028431@qq.com>
2019-09-28 11:00:41 +0800

    aa

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::any('event','EventController@event'); // 微信第一次设置url端口使用
+Route::get('wechat/get_access_token','WechatController@get_access_token'); //获取access_token
+Route::get('/wechat/get_user_list','WechatController@get_user_list'); //获取用户列表
+Route::get('/wechat/get_user_xiang','WechatController@get_user_xiang'); //详情列表v
